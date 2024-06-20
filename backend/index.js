31f102f13de9b07e2ff874fb9845c1ed98f6b33e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-20 14:31:45 +0100

    sstarting

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { PORT } from './config.js';
 
-const app = express();
+const app = express(); //express is our backend framework
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send("App is live")
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening on PORT: ${PORT}`);
