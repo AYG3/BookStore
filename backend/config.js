bf3b245c2e766a5c42563844b6ae3ff35046c185
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-21 10:01:45 +0100

    working

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://gilbertayoku3:XZ3mZSCSLI8Jbnnm@cluster0.c997hrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// export const mongoDBURL = 'mongodb+srv://gilbertayoku3:XZ3mZSCSLI8Jbnnm@cluster0.c997hrt.mongodb.net/books-collectionretryWrites=true&w=majority&appName=Cluster0' - Added this in tutorial
