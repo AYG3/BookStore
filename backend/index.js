5b2c84c760789fbd24bdfd84ad5f0576151ab64b
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-21 10:09:17 +0100

    working

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { PORT, mongoDBURL } from './config.js';
+import mongoose from "mongoose";
 
 const app = express(); //express is our backend framework
 
@@ -8,6 +9,20 @@ app.get('/', (request, response) => {
     return response.status(234).send("App is live")
 })
 
-app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`App is listening on PORT: ${PORT}`);
+// });
+
+//Conect to database
+mongoose.connect(mongoDBURL)
+.then(() => {
+    console.log('App is conected to database')
+
+    //To ensure app only runs when conected to database
+    app.listen(PORT, () => {
+        console.log(`App is listening on PORT: ${PORT}`);
+    });
+})
+.catch((error) => {
+    consdole.log(error)
+})
