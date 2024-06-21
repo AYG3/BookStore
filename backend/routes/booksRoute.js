42340e74c41ff9d525a6e2d61bf5c62024f41a95
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-21 14:17:54 +0100

    progress

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -54,7 +54,8 @@ router.get('/:id', async (request, response) => {
 
     } catch (error) {
         console.log(error.message)
-        return response.status(500).send(error.message)
+        // return response.status(500).send({message : error.message})
+        return response.status(500).send("Didn't see your link friend :(")
     }
 });
 
