60d32af893a1dd425399fab5355f4883f200204b
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-23 19:17:40 +0100

    progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import EditBook from './pages/EditBook'
+import Showbook from './pages/Showbook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBook from './pages/CreateBook'
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={ <CreateBook /> } />
+      <Route path='/books/details/:id' element={ <Showbook /> } />
+      <Route path='/books/edit/:id' element={ <EditBook /> } />
+      <Route path='/books/delete/:id' element={ <DeleteBook /> } />
     </Routes>
   )
 }
