19442576964587334ab993d594ebee1e37a9ca8e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-23 19:38:28 +0100

    progress

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import Spinner from '../components/Spinner'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
-const Home = () => {
+
+const Home = () => { 
+    const [books, setBooks ] = useState([])
+    const [loading, setLoading] = useState(false)
+
   return (
     <div>
       
