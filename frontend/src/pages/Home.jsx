3def6a9bdb561c3b1cbc9ebbe889cb85361f738d
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-23 20:02:11 +0100

    progress

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,8 +23,50 @@ const Home = () => {
         })
     })
   return (
-    <div>
-      
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'> Books List </h1>
+        <Link  to='books/create'>
+            <MdOutlineAddBox className='text-sky-800 text-4x1' />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ):(
+        <table className='w-full border-seperate border-spacing-2'>
+            <thead>
+                <tr>
+                    <th className='border O border-slate-600 rounded-md'>No</th>
+                    <th className='border O border-slate-600 rounded-md'>Title</th>
+                    <th className='border O border-slate-600 rounded-md max-md:hidden'>Author</th>
+                    <th className='border O border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+                    <th className='border O border-slate-600 rounded-md'>Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                {books.map((book, index) =>{
+                    <tr key={books._id} className='h-8'>
+                        <td className='border-slate-700 rounded-md text-center'>
+                            {index + 1}
+                        </td>
+                        <td className='border-slate-700 rounded-md text-center'>
+                            {book.title}
+                        </td>
+                        <td className='border-slate-700 rounded-md text-center max-md:hidden'>
+                            {book.author}
+                        </td>
+                        <td className='border-slate-700 rounded-md text-center max-md:hidden'>
+                            {book.publishYear}
+                        </td>
+                        <td className='border-slate-700 rounded-md text-center'>
+                            <div className='flex justify-center gap-'></div>
+                        </td>
+                    </tr>
+                })}
+            </tbody>
+
+        </table>
+      )}
     </div>
   )
 }
