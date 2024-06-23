846511455e1594f2d69b3208a322e2c1e297efbb
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-23 19:43:19 +0100

    progress

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Spinner from '../components/Spinner'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { BsInfoCircle } from 'react-icons/bs'
+import  AiOutlineEdit from 'react-icons/ai'
+import  BsInfoCircle  from 'react-icons/bs'
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 
 const Home = () => { 
     const [books, setBooks ] = useState([])
     const [loading, setLoading] = useState(false)
-
+    useEffect(() => {
+        setLoading(true)
+        axios.get('http:localhost:5555/books')
+        .then((response) => {
+            setBooks(response.data.data);
+            setLoading(false)
+        })
+        .catch((error) => {
+            console.log(error)
+            setLoading(false)
+        })
+    })
   return (
     <div>
       
