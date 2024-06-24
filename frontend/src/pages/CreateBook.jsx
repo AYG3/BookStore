3c6fe6c4ac91fe1560e41d039bb4014f708aff1a
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-24 15:32:15 +0100

    progress

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+
 
 const CreateBook = () => {
+  const [ title, setTitle ] = useState('')
+  const [ author, setAuthor ] = useState('')
+  const [ publishYear, setpublishYear ] = useState('')
+  const navigate = useNavigate() //to navigate to main page after book is created
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    }
+    setLoading(true)
+    axios.get('http://localhost:5555/books', data) //pass in the data as the secind argument
+    .then((response) => {
+      setLoading(false)
+      navigate('/')
+    })
+    .catch((error) => {
+      setLoading(false)
+      alert('Error: pls check console')
+      console.log(error)
+    })
+  }
+
   return (
-    <div>
+    <div className=''>
       CreateBookser
     </div>
   )
