33eaf12c84beaef10e5baccc66b9675ca49504c9
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-24 13:16:30 +0100

    progress

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsArrowLeft} from 'react-icons/bs'
+
+const BackButton = ( {destination = '/'} ) => { //default is destination if nothing is passed into it when called
+  return (
+    <div>
+      <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'>
+        <BsArrowLeft />
+      </Link>
+    </div>
+  )
+}
+
+export default BackButton;
