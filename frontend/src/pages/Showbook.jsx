17aa9d0a7e18d6ffe93507b2f40f44bf6ecc3619
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-24 15:02:59 +0100

    progress

--- frontend/src/pages/Showbook.jsx
+++ frontend/src/pages/Showbook.jsx
@@ -41,6 +41,18 @@ const Showbook = () => {
             <span className='text-xl mr-4 0 text-gray-500'>Author </span > 
             <span>{book.author}</span>
           </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 0 text-gray-500'>Publish Year </span > 
+            <span>{book.publishYear}</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 0 text-gray-500'>Create Time </span > 
+            <span>{ new Date(book.createdAt).toString() }</span>
+          </div>
+          <div className='my-4'>
+            <span className='text-xl mr-4 0 text-gray-500'>Last Update Time </span > 
+            <span>{ new Date(book.createdAt).toString() }</span>
+          </div>
         </div>
       )}
       
