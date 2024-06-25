d78f4774eab0d62435524cb22aa12a0a3b3c80c9
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-25 08:59:16 +0100

    progress

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -37,18 +37,21 @@ const CreateBook = () => {
       { loading ? <Spinner /> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className='my-4'>
-          <label className='text-xl mr-4  text-gray-500' />
+          <label className='text-xl mr-4  text-gray-500'> Title </label>
           <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4  text-gray-500' />
+          <label className='text-xl mr-4  text-gray-500' >Author</label>
           <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
         </div>
         <div className='my-4'>
-          <label className='text-xl mr-4  text-gray-500' />
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
+          <label className='text-xl mr-4  text-gray-500' >Publish Year</label>
+          <input type="text" value={publishYear} onChange={(e) => setpublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full'/>
         </div>
 
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
       </div>
     </div>
   )
