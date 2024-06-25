5bf3d7b8a94b2d372da58bbe3503e72dada7f9cc
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-25 10:23:07 +0100

    page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,8 @@ import Spinner from '../components/Spinner'
 import  {AiOutlineEdit} from 'react-icons/ai'
 import  {BsInfoCircle}  from 'react-icons/bs'
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import table from '../components/home/BooksTable'
+import Card from '../components/home/BooksCard'
 
 
 const Home = () => { 
@@ -28,8 +30,8 @@ const Home = () => {
   return (
     <div className='p-4'>
         <div className='flex justify-center items-center gap-x-4'>
-            <button className='bg-sky-300 hover:bg-sky-600 px-4 py-l rounded-lg' onClick={setShowType('table')}> Table</button>
-            <button className='bg-sky-300 hover:bg-sky-600 px-4 py-l rounded-lg' onClick={setShowType('Card')}> Card</button>
+            <button className='bg-sky-300 hover:bg-sky-600 px-4 py-l rounded-lg' onClick={() => {setShowType('table')}}> Table</button>
+            <button className='bg-sky-300 hover:bg-sky-600 px-4 py-l rounded-lg' onClick={() => {setShowType('card')}}> Card</button>
         </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'> Books List </h1>
