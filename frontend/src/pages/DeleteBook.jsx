febaaa7356295ea2fbe50557c8b73c73bbea2974
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-25 10:05:10 +0100

    progress

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -23,9 +23,13 @@ const DeleteBook = () => {
     })
   }
   return (
-    <div>
-      <h1 className='text-3xl p-4'>DeleteBook</h1>
-      <button className='' onClick={handleDeleteBook}>Delete</button>
+    <div className='p-4'>
+      <h1 className='text-3xl my-4'>DeleteBook</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='border-sky-400 rounded-xl w-[600px] p-8 flex flex-col items-center border-2'>
+        <h3 className='text-2xl'> Are you sure you want to delete this book</h3>
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Delete</button> 
+      </div>
     </div>
   )
 }
