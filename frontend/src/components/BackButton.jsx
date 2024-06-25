f20e0e4860596a629490a6d0c6154c529afb1245
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-25 08:44:52 +0100

    progress

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsArrowLeftSquareFill } from "react-icons/bs";
-import { PiArrowSquareLeftFill } from "react-icons/pi";
 
 const BackButton = ( {destination = '/'} ) => { //default is destination if nothing is passed into it when called
   return (
-    <div>
-      <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'>
-      <PiArrowSquareLeftFill />
-      </Link>
+    <div className='p-8'>
+      <Link to={destination} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'><BsArrowLeftSquareFill /></Link>
     </div>
   )
 }
