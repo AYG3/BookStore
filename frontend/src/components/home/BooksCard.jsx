945381f413f28296ba79f7a58d21501144913312
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-29 18:52:54 +0100

    chore: Add empty div in BooksCard component

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const BooksCard = ( { books} ) => {
   return (
     <div className='p-4'>
+      <div className='p-4'>
+
+      </div>
       Card  for books to
     </div>
   )
