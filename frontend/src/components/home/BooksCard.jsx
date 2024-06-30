00240860f7e106ac60e937bb948ac201894482ff
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-30 18:51:04 +0100

    progress

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const BooksCard = ( { books} ) => {
   return (
-    <div className='p-4'>
-      <div className='p-4 border border-gray-600'>
-
-      </div>
-      Card  for books to
+    <div className='grid sm:grid-cols-2 lg:grid-cols-3 x1:grid-cols-4'>
+        {books.map((item) => (
+        <div key={item._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-x1'>
+          <p>{item.name}</p>  
+        </div>
+        ))}
     </div>
   )
 }
