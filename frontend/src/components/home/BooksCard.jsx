79b543af090a602b68e9759d8c7ec72dcae3f4d2
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-30 17:10:28 +0100

    chore: Add border to BooksCard component

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BooksCard = ( { books} ) => {
   return (
     <div className='p-4'>
-      <div className='p-4'>
+      <div className='p-4 border border-gray-600'>
 
       </div>
       Card  for books to
