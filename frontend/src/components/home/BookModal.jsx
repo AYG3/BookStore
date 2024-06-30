096eb03529e56708c9b5211b2ef3b72c05b61f5f
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-30 19:41:58 +0100

    chore: Update BookModal component styling

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -6,7 +6,7 @@ const BookModal = ( { book, onClose }) => {
   return (
     <div className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center" onClick={onClose}>
       <div onClick={(event) => EventTarget.stopPropagation()} className="bg-white rounded-xl p-4 flex flex-col relative w-[600px] max-w-full h-[400px]">
-
+    
       </div>
     </div>
   )
