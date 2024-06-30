078eba748cb5d1cc6965dd9902750e277b5a99a4
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-30 19:27:00 +0100

    progrss

---
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+import { BsInfoCircle } from 'react-icons/bs';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { MdDeleteOutline } from 'react-icons/md';
+
+const BookSingleCArd = ( {book} ) => {
+  return (
+    <div key={book._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-x1'>
+          <h1 className='absolute top-1 right-2 px-4 py-l bg-red-300 rounded-lg'>{book.publishYear}</h1>
+
+          <h4 className='my-2 text-gray-500'>{book._id} </h4>
+          <div className='flex justify-start books-center gap-x-2'>
+            <PiBookOpenTextLight className='text-red-300 text-2xl' />
+            <h2 className='my-2'> {book.title}</h2>
+          </div>
+
+          <div className='flex justify-start books-center gap-x-2'>
+            <BiUserCircle className='text-red-300 text-2xl' />
+            <h2 className='my-1'>{book.author}</h2>
+          </div>
+
+          <div className='flex justify-between books-center gap-x-2 mt-4 pt-4'>
+            <Link to={`/books/detail/${book._id}`}>
+              <BsInfoCircle className='text-green-800 text-2xl hover:text-black' />
+            </Link>
+            <Link to={`/books/edit/${book._id}`}>
+              <AiOutlineEdit className='text-yellow-600 hover:text-black text-2x1' />
+            </Link>
+            <Link to={`/books/delete/${book._id}`}>
+              <MdDeleteOutline className='text-2xl text-red-600 hover:text-black' />
+            </Link>
+          </div>
+
+        </div>
+  )
+}
+
+export default BookSingleCArd
--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,45 +1,18 @@
-import React from 'react'
-import Link from 'react-router-dom'
-import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi'
-import { BsInfoCircle } from 'react-icons/bs'
-import { AiOutlineEdit } from 'react-icons/ai'
-import { MdDeleteOutline } from 'react-icons/md'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+import { BsInfoCircle } from 'react-icons/bs';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { MdDeleteOutline } from 'react-icons/md';
+import BookSingleCard from './BookSingleCard';
 
-const BooksCard = ( { books} ) => {
+export const BooksCard = ({ books }) => {
   return (
     <div className='grid sm:grid-cols-2 lg:grid-cols-3 x1:grid-cols-4'>
-        {books.map((item) => (
-        <div key={item._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-x1'>
-          <h1 className='absolute top-1 right-2 px-4 py-l bg-red-300 rounded-lg'>{item.publishYear}</h1>  
-
-          <h4 className='my-2 text-gray-500'>{item._id} </h4>
-          <div className='flex justify-start items-center gap-x-2'>
-            <PiBookOpenTextLight  className='text-red-300 text-2xl'/>
-            <h2 className='my-2' > {item.title}</h2>
-          </div>
-
-          <div className='flex justify-start items-center gap-x-2'>
-            <BiUserCircle className='text-red-300 text-2xl' />
-            <h2 className='my-1'>{item.author}</h2>
-          </div>
-
-          <div className='flex justify-start items-center gap-x-2 mt-4 pt-4'>
-            <Link to={`/books/detail/${item._id}`} >
-              <BsInfoCircle className='text-green-800 text-2xl hover:text-black' />
-            </Link>
-            <Link to={`/books/edit/${item._id}`} >
-              <AiOutlineEdit className='text-yellow-600 hover:text-black text-2x1' />
-            </Link>
-            <Link to={`/books/delete/${item._id}`} >
-              <MdDeleteOutline className='text-2xl text-red-600 hover:text-black' />
-            </Link>
-          </div>
-
-        </div>
-        ))}
+      {books.map((item) => (
+        <BookSingleCard book={item} />
+      ))}
     </div>
-  )
-}
-
-export default BooksCard
+  );
+};
