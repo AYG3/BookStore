8014941bfcc5f0ded503700abaef22236984832f
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-30 19:41:05 +0100

    chore: Add click event handler to BookModal component

--- frontend/src/components/home/BookModal.jsx
+++ frontend/src/components/home/BookModal.jsx
@@ -5,7 +5,7 @@ import { BiUserCircle } from "react-icons/bi"
 const BookModal = ( { book, onClose }) => {
   return (
     <div className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center" onClick={onClose}>
-      <div onClick={(event) => EventTarget.stopPropagation()}>
+      <div onClick={(event) => EventTarget.stopPropagation()} className="bg-white rounded-xl p-4 flex flex-col relative w-[600px] max-w-full h-[400px]">
 
       </div>
     </div>
