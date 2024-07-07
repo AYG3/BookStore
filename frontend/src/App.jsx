bfb4afcd94a892addb492029bd221e01a05b4024
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-07-07 20:24:44 +0100

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,9 +16,6 @@ const App = () => {
       <Route path='/books/edit/:id' element={ <EditBook /> } />
       <Route path='/books/delete/:id' element={ <DeleteBook /> } />
     </Routes>
-    // <div className='text-5xl font-bold bg-red-500'>
-    //    <p className='text-7xl font-bold text-emerald-500'>Checkers</p>
-    // </div>
   )
 }
 
